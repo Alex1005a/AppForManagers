e29c38afe8e3b6e17f3f9f45c8fcbef212adb829
Rosia <azazf932gmail.com>
2021-01-18 21:09:26 +0300

    Action.asyncF

--- app/controllers/AccountController.scala
+++ app/controllers/AccountController.scala
@@ -2,82 +2,68 @@ package controllers
 
 import Formats.JsonFormats.{ManagerDtoFormat, WorkerDtoFormat}
 import auth.Authentication
+import cats.effect.{ContextShift, IO}
+import controllers.IOHttp.ActionBuilderOps
 import models.{UnverifiedManager, Worker}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import repositories.UserRepository
 import services.{AccountService, AuthorizeConfig}
+
 import java.util.UUID
 import javax.inject.Inject
+import scala.concurrent.ExecutionContext
 
 abstract class AuthDto
 case class ManagerDto(name: String, email: String, password: String) extends AuthDto
 case class WorkerDto(name: String, password: String) extends AuthDto
 
-class AccountController @Inject()(cc: ControllerComponents, auth: Authentication, repo: UserRepository) extends AbstractController(cc) {
-  def create: Action[AnyContent] = Action { implicit request =>
+class AccountController @Inject()(cc: ControllerComponents, auth: Authentication, repo: UserRepository) (implicit ec: ExecutionContext) extends AbstractController(cc) {
+  implicit val cs: ContextShift[IO] = IO.contextShift(ec)
+
+  def create: Action[AnyContent] = Action.asyncF { implicit request =>
     request.body.asJson match {
       case Some(v) if v.validate[ManagerDto].isSuccess =>
         val m = v.validate[ManagerDto].get
-        UnverifiedManager(m.name, m.email, m.password, UUID.randomUUID().toString).fold(
-          err => InternalServerError(err),
-          manager => Ok(AccountService.createUser(manager).run(repo).unsafeRunSync())
-        )
+        UnverifiedManager(m.name, m.email, m.password, UUID.randomUUID().toString) match {
+          case Left(err) => IO.pure(InternalServerError(err))
+          case Right(manager) => AccountService.createUser(manager).run(repo).map(r => Ok(r))
+        }
 
       case Some(v) if v.validate[WorkerDto].isSuccess =>
         val w = v.validate[WorkerDto].get
-        Worker(w.name, w.password).fold(
-          err => InternalServerError(err),
-          worker => Ok(AccountService.createUser(worker).run(repo).unsafeRunSync())
-        )
+        Worker(w.name, w.password) match {
+          case Left(err) => IO.pure(InternalServerError(err))
+          case Right(worker) => AccountService.createUser(worker).run(repo).map(r => Ok(r))
+        }
 
-      case Some(_) => InternalServerError("Parse json error")
+      case Some(_) => IO.pure(InternalServerError("Parse json error"))
 
-      case None => Ok("Missing or incorrect body")
+      case None => IO.pure(Ok("Missing or incorrect body"))
     }
   }
 
-  def verifyManager(confirmationToken: String): Action[AnyContent] = Action {
-    AccountService.verifyManager(confirmationToken).run(repo).map { result =>
-      result.fold(
-        err => InternalServerError(err),
-        res => Ok(res)
-      )
-    }.unsafeRunSync()
+  def verifyManager(confirmationToken: String): Action[AnyContent] = Action.asyncF { implicit request =>
+    AccountService.verifyManager(confirmationToken).run(repo).map {
+      case Left(err) => InternalServerError(err)
+      case Right(res) => Ok(res)
+    }
   }
 
-  def authorize: Action[AnyContent] = Action { implicit request =>
+  def authorize: Action[AnyContent] = Action.asyncF { implicit request =>
     request.body.asJson match {
       case Some(v) if v.validate[ManagerDto].isSuccess =>
         val m = v.validate[ManagerDto].get
-        Ok(serviceAuthorize(m).unsafeRunSync())
+        serviceAuthorize(m).map(r => Ok(r))
 
       case Some(v) if v.validate[WorkerDto].isSuccess =>
         val w = v.validate[WorkerDto].get
-        Ok(serviceAuthorize(w).unsafeRunSync())
+        serviceAuthorize(w).map(r => Ok(r))
 
-      case Some(_) => InternalServerError("Parse json error")
+      case Some(_) => IO.pure(InternalServerError("Parse json error"))
 
-      case None => Ok("Missing or incorrect body")
+      case None => IO.pure(Ok("Missing or incorrect body"))
     }
   }
 
   private def serviceAuthorize(dto: AuthDto) =  AccountService.authorize(dto).run(AuthorizeConfig(repo, auth)).map(_.fold(c => c, f => f))
 }
-
-/*
-request.body.asJson match {
-      case Some(v) => v.validate[WorkerDto].fold(
-        _ => v.validate[ManagerDto].fold(
-          _ => InternalServerError("Parse json error"),
-          m => Manager(m.name, m.email, m.password).fold(
-            err => InternalServerError(err),
-            manager => Ok(AccountService.createUser(manager).run(repo))
-          )),
-        w => Worker(w.name, w.password).fold(
-          err => InternalServerError(err),
-          worker => Ok(AccountService.createUser(worker).run(repo))
-        ))
-        InternalServerError("Parse json error")
-      case None => Ok("Missing or incorrect body")
-    }
- */
